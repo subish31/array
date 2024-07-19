00e444a9a618a28d6966c7cc25f7e6525e90a496
Subish Kumar A <156400422+subish31@users.noreply.github.com>
2024-07-19 17:08:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-// 1
-let arr=[1,2,3,4,5];
-console.log(arr);
+//7 array joining and splitting
 
-//2
-arr.push(6,7);
-console.log(arr);
-arr.unshift(0);
-console.log(arr);
-arr.pop();
-console.log(arr);
-arr.shift();
-console.log(arr);
-
-//3
-console.log(arr.indexOf(4));
-let lar=(element)=>element>3;
-console.log(arr.find(lar));
-console.log(arr.includes(2));
-
-//4
-let even=n=>n%2==0;
-console.log(arr.filter(even));
-
-//5
-let number=[4,9,16,25,36];
-let n=number.map(Math.sqrt);
-console.log(n);
-
-//6
-let arr1=[27,12,31]
-console.log(arr1.sort());
-console.log(arr1.reverse());
+let string=['Hello','World'];
+let news=string.join(' ');
+console.log(news);
+let spl=news.split(" ");
+console.log(spl);
 
 
-//10
-let num=[88,70,83,46];
-let max=num[0];
-let min=num[0];
-for(let i=0;i<num.length;i++)
-{
-    if(max<num[i])
-        max=num[i];
-    if(min>num[i])
-        min=num[i];
-}
-console.log("Max : ",max);
-console.log("Min : ",min);
+//8 array destruction
+
+let num=["hai","hello","haii","hey"];
+const[first,second, , fourth]=num;
+console.log(first);
+console.log(second);
+console.log(fourth);
+
+
+//9 array spreading
+
+let arr=[1,2,3];
+let ar=[4,5,6];
+let array=[...arr,...ar];
+document.write(array);
+
+//11 Array flattening
+let nest=[1,[2,[3,[4]]]];
+let flat=nest.flat(Infinity);
+console.log(flat);
+
+
+//12 checking array elements
+const arra=[27,12,31,98,94]
+console.log(arra.includes(27));
+console.log(arra.some(n=>n%2==0));
+console.log(arra.find(n=>n%2==0));
+
+//13.sorting an array object
+
+const arr3 = [
+    { name:'John',age:19},
+    { name:'Jane',age:30},
+    { name:'Bob',age:20},
+    { name:'sam',age:21}
+  ];
+  arr3.sort((a, b) => a.age - b.age); 
+  console.log(arr3);
+
