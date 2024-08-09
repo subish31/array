a112dde6d604c23003fa751765fbc69d9ce139c2
Subish Kumar A <156400422+subish31@users.noreply.github.com>
2024-08-09 10:36:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -90,20 +90,6 @@ function longest(str) {
 }
 console.log(longest(st));
 
-
-// 6 factorial of a given  number
-
-function fact(n)
-{
-    for(let i=1;i<=n;i++)
-    {
-        if(n%i==0)
-            console.log(i);
-    }
-}
-fact(98);
-
-
 //6 array sorting and reverse
 let arr3=[27,12,31]
 console.log(arr3.sort());
@@ -181,3 +167,158 @@ let arr1=[1,2,3];
 let arr2=[4,5,6];
 let arr=arr1.concat(arr2);
 console.log(arr);
+
+// 18.intersection of two array
+let a=[1,2,3,4,5,7,6]
+let b=[2,4,6,8,10,12,14]
+let c=[]
+for(let i=0;i<a.length;i++){
+    for(let j=0;j<b.length;j++){
+        if(a[i]==b[j]){
+            c.push(a[i])
+        }
+    }
+}
+console.log(c)
+
+// 19.difference b/w 2 array
+
+let ar=[1,2,3,4,5,7,6]
+let arr1=[2,4,6,8,10,12,14]
+let arr2=ar.filter(x => !arr1.includes(x));
+let arr3=arr1.filter(x => !ar.includes(x));
+let arr4=arr2.concat(arr3)
+console.log(arr4)
+
+//20.grouping array element
+
+const arr5 = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4];
+const groupedArr = arr5.reduce((acc, current) => {
+    if (!acc[current]) {
+        acc[current] = [];
+    }
+    acc[current].push(current);
+    return acc;
+}, {});
+console.log(groupedArr);
+
+// 21.array intersection of two set
+
+const array1 = [1, 2, 3];
+const array2 = [2, 3, 4, 5];
+const farray = array1.filter(value => array2.includes(value));
+console.log(farray)
+
+//22.array manipulation with slice()
+
+const arr7 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+arr7.splice(2, 3); 
+console.log(arr7);
+
+// 23.implement queue with array 
+const queue = [];
+queue.push(2);
+queue.push(3);
+queue.push(4);
+queue.push(5);
+queue.push(6);
+const dequeuedItem = queue.shift(); 
+console.log(queue);
+
+// 24.implement stack with array
+const stack = [];
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+stack.push(6);
+const poppedItem = stack.pop();
+console.log(stack);
+
+//25.array of object sorting with multiple properties
+const arr = [
+    { name: 'ajay', age: 20},
+    { name: 'tom', age: 30},
+    { name: 'sam', age: 18},
+    { name: 'ram', age: 30 }
+];
+arr.sort((a, b) => {
+    if (a.age < b.age) 
+        return -1;
+    if (a.age > b.age) 
+        return 1;
+});
+console.log(arr);
+//26.creating array of arrays
+const arrayOfArrays = [];
+arrayOfArrays.push([1, 2, 3]);
+arrayOfArrays.push([4, 5, 6]);
+arrayOfArrays.push([7, 8, 9]);
+console.log(arrayOfArrays);
+
+//27.comparing two array of equality
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3];
+const arr3 = [1, 2, 4];
+console.log("arr1 : "+arr1);
+console.log("arr2 : "+arr2);
+console.log("arr3 : "+arr3);
+function check(a1,a2){
+    c=0;
+    if(a1.length!==a2.length){
+        return false
+    }
+    for(let i=0;i<a1.length;i++){
+        if(a1[i]==a2[i]){
+            c++;
+        }
+    }
+    if(c==a1.length)
+        return true;
+    else
+        return false;
+}
+console.log("arr1 == arr2 : "+check(arr1,arr2));
+console.log("arr1 == arr3 : "+check(arr1,arr3));
+
+ // 28.Convert array into object
+var arr=['a','b','c','d','e','f'];
+var obj={}
+for(var i=0;i<arr.length;i++){
+    obj[arr[i]]=arr[i];
+}
+console.log("Convert array into object : ",obj);
+
+//29 . removing array elements bys index;
+let arr=[27,12,3,31];
+let index=2;
+arr.splice(index,1);
+console.log(arr);
+
+// 30.Insert item at specific position
+const arr2=['a','b','c','d','e','f'];
+arr2.splice(4, 0, "g");
+console.log("Insert item at specific position : ",arr2.join());
+
+//31.Sorting array with complex object
+const arr3 = [
+    { name: 'Ravi', age: 25 },
+    { name: 'Kappa', age: 30 },
+    { name: 'Sriram', age: 20 },
+    { name: 'Kumar', age: 35 }
+];
+arr3.sort((a, b) => a.age - b.age);
+console.log("Sorting array with complex object : ", arr3);
+
+//.32 Longest string in an array
+const arr4=["Ravi","Kappa","Sriram","Samy","subish","Kannan","Mams"]
+let me=arr4[0].length;
+for(var j=0;j<arr4.length;j++)
+{
+if(arr4[j].length>me)
+    {
+    me=arr4[j].length;
+    var indx=j;
+    }
+}
+console.log("Longest string in an array : ",arr4[indx])
